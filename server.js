cc3864ac7689bd629cfb99e56618d6f634f33a92
FateXRebirth <FateXRebirth@gmail.com>
2019-08-18 23:36:28 +0800

    GraphQL With React & Apollo [2] - React & Apollo Setup

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require("./schema");
 const app = express();
+
+// Allow Cross-Origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
