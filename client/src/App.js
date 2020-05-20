8081b43a36b204d2474445473592c5dc43e7b0cd
FateXRebirth <FateXRebirth@gmail.com>
2020-05-21 07:43:50 +0800

    GraphQL With React & Apollo [3] - Finishing The App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import Launches from './component/Launches';
+import Launch from './component/Launch';
 import logo from './spacex.jpg';
 import './App.css';
 
@@ -13,10 +15,13 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto'}} />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto'}} />
+            <Route exact path="/" component={Launches}></Route>
+            <Route exact path="/launch/:flight_number" component={Launch}></Route>
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
